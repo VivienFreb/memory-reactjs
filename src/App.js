a9edbe8b6949be7f157bcec9290e0c7982817bc8
VivienFreb <vivien.frebourg@outlook.com>
2020-08-05 23:14:11 +0200

    Gestion de la vérification lors de la sélection de deux cartes. Bug: Possiblité de cliquer deux fois sur la même carte = une paire. Todo: Guesses count

--- src/App.js
+++ src/App.js
@@ -8,11 +8,17 @@ import GuessCount from './GuessCount'
 
 import HallOfFame, {FAKE_HOF} from "./HallOfFame";
 
-const SIDE = 15
+const SIDE = 3
 const SYMBOLS = '🍕🍔🍟🌭🍿🧂🥓🥚🥯🥨🥐🍞🧈🥞🧇🍳🥖🧀🥗🥙🥪🌮🌯🥫🍱🥡🥠🍠🥟🥩🍗🍖🍘🍙🍚🍛🍜🦪🍣🍤🍥🥮🍢🧆🥘🍲🍝🥣🍰🎂🍪🍩🍨🍧🍦🥧🧁🍫🍬🍭🍡🍮🍯🍼🍷🍾🍶🧉🍵☕🧃🥛🍸🍹🍺🍻🥂🥃🧊🥤🍇🥥🥝🏺🥄🍴🍽🥢🍈🍉🍊🍋🍌🍍🥭🍎🌽🍆🍅🍓🍒🍑🍐🍏🌶🍄🥑🥒🥬🥦🥔🧄🌹🏵🌸💐🥜🌰🥕🧅🌺🌻🌼🌷🥀☘🌱🌲🍂🍁🍀🌿🌾🌵🌴🌳🍃'
+const PAUSE_MS = 750
 
 class App extends Component {
-    plateauCards = this.generateCards()
+    state = {
+        plateauCards: this.generateCards(),
+        currentPair: [],
+        guesses: 0,
+        matchedCardIndices: [],
+    }
 
     generateCards() {
         const result = []
@@ -25,19 +31,61 @@ class App extends Component {
         return shuffle(result)
     }
 
-    handleCardClick(card) {
-        console.log(card, 'clicked')
+    getCardFeedbackFor(index){
+        const { currentPair, matchedCardIndices } = this.state
+        const indexMatched = matchedCardIndices.includes(index)
+
+        if(currentPair.length < 2){
+            return indexMatched || index === currentPair[0] ? 'visible' : 'hidden'
+        }
+
+        if(currentPair.includes(index)){
+            return indexMatched ? 'justMatched' : 'justMismatched'
+        }
+
+        return indexMatched ? 'visible' : 'hidden'
     }
 
+    handleCardClick = index => {
+        const { currentPair } = this.state
+
+        if(currentPair.length === 2){
+            return
+        }
+
+        if (currentPair.length === 0){
+            this.setState({currentPair: [index] })
+            return
+        }
+
+        this.handleNewPairClosedBy(index)
+    }
+
+    handleNewPairClosedBy(index){
+        const { plateauCards, currentPair, guesses, matchedCardIndices } = this.state
+        const newPair = [currentPair[0], index]
+        const newGuesses = guesses + 1
+        const matched = plateauCards[newPair[0]] === plateauCards[newPair[1]]
+        this.setState({currentPair: newPair, guesses: newGuesses})
+        if(matched){
+            this.setState({matchedCardIndices: [...matchedCardIndices, ...newPair] })
+        }
+        setTimeout(() => this.setState({currentPair: [] }), PAUSE_MS)
+    }
+
+
+
     render() {
-        const won = new Date().getSeconds() % 2 === 0
+        const { plateauCards, guesses, matchedCardIndices } = this.state
+        const won = matchedCardIndices.length === plateauCards.length
         return (
             <div className="memory">
                 <GuessCount guesses={0} />
-                {this.plateauCards.map((card, index) => (
+                {plateauCards.map((card, index) => (
                     <Card
                         card={card}
-                        feedback="visible"
+                        feedback={this.getCardFeedbackFor(index)}
+                        index={index}
                         key={index}
                         onClick={this.handleCardClick}
                     />
