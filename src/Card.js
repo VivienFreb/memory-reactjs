da17ff29f3de6d2f5d391edbdc7655049ed80f00
VivienFreb <vivien.frebourg@outlook.com>
2020-08-05 16:22:11 +0200

    Génération aléatoire du plateau (map) Affichage d'un faux leaderboards

--- src/Card.js
+++ src/Card.js
@@ -3,8 +3,8 @@ import './Card.css'
 
 const HIDDEN_SYMBOL = '❓';
 
-const Card = ({ card, feedback }) => (
-    <div className={`card ${feedback}`}>
+const Card = ({ card, feedback, onClick }) => (
+    <div className={`card ${feedback}`} onClick={() => onClick(card)}>
     <span className="symbol">
       {feedback === 'hidden' ? HIDDEN_SYMBOL : card}
     </span>
