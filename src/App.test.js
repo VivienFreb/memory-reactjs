5f9c1badc004bfd3e974095217c7f0dc3218ddff
VivienFreb <vivien.frebourg@outlook.com>
2020-08-07 14:14:14 +0200

    Utilisation test lié à un événement

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { shallow, mount, render } from 'enzyme';
+import { shallow } from 'enzyme';
 
 import App from './App'
+import Card from './Card'
 
 describe('<App/>', () => {
   it('renders without crashing', () => {
     const wrapper = shallow(<App />);
+  });
+  describe('<Card/>', () => {
+    it('testing onClick event', () => {
+      const onClick = jest.fn();
+      const wrapper = shallow(<Card card="😁" feedback="hidden" index={0} onClick={onClick} />);
+
+      wrapper.simulate('click')
+      expect(onClick).toHaveBeenCalledWith(0)
+    })
   })
 });
